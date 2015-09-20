7fa022f71795eebafc590e353cde9ce385d4b1fd
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-09-20 17:11:17 -0400

    first commit, hopefully added source files

---
+++ com/codingbat/results/Warmup_1.java
@@ -0,0 +1,91 @@
+package com.codingbat.results;
+
+public class Warmup_1 {
+
+	public boolean sleepIn(boolean weekday, boolean vacation) {
+		return (!weekday || vacation); 
+	}
+
+	public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+		return (aSmile && bSmile) || (!aSmile && !bSmile);
+	}
+
+	public int sumDouble(int a, int b) {
+		if (a == b) {
+			return (a + b)*2; // double sum
+		}
+		return a + b;
+	}
+
+	public int diff21(int n) {
+		if (n > 21) {
+			return Math.abs(n-21)*2;
+		}
+		return Math.abs(21-n);
+	}
+
+	public boolean parrotTrouble(boolean talking, int hour) {
+		if (talking && (hour < 7 || hour > 20)) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean makes10(int a, int b) {
+		return a == 10 || b == 10 || (a+b) == 10;
+	}
+
+	public boolean nearHundred(int n) {
+		if (Math.abs(100-n) <= 10 || Math.abs(200-n) <= 10) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean posNeg(int a, int b, boolean negative) {
+		if (negative) {
+			return (a < 0 && b < 0);
+		}
+		return (a > 0 && b < 0) || (a < 0 && b > 0);
+	}
+	
+	public String notString(String str) {
+		if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+			return str;
+		}
+		return "not " + str;
+	}
+
+	public String missingChar(String str, int n) {
+		return str.substring(0, n) + str.substring(n+1);
+	}
+
+	public String frontBack(String str) {
+		if (str.length() <= 1) {
+			return str;
+		}
+		String first, mid, last;
+		first = str.substring(0,1);
+		last = str.substring(str.length()-1);
+		mid = str.substring(1,str.length()-1);
+		
+		return last + mid + first;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ com/codingbat/results/Warmup_1Test.java
@@ -0,0 +1,102 @@
+package com.codingbat.results;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Warmup_1Test {
+
+	Warmup_1 obj = new Warmup_1();
+
+	@Test
+	public void testSleepIn() {
+		assertTrue(obj.sleepIn(false, false));
+		assertFalse(obj.sleepIn(true, false));
+		assertTrue(obj.sleepIn(false, true));
+	}
+
+	@Test
+	public void testMonkeyTrouble() {
+		assertTrue(obj.monkeyTrouble(true, true));
+		assertTrue(obj.monkeyTrouble(false, false));
+		assertFalse(obj.monkeyTrouble(true, false));
+	}
+
+	@Test
+	public void testSumDouble() {
+		assertEquals(3, obj.sumDouble(1, 2));
+		assertEquals(5, obj.sumDouble(3, 2));
+		assertEquals(8, obj.sumDouble(2, 2));
+	}
+
+	@Test
+	public void testdiff21() {
+		assertEquals(2, obj.diff21(19));
+		assertEquals(11, obj.diff21(10));
+		assertEquals(0, obj.diff21(21));
+	}
+
+	@Test
+	public void testParrotTrouble() {
+		assertTrue(obj.parrotTrouble(true, 6));
+		assertFalse(obj.parrotTrouble(true, 7));
+		assertFalse(obj.parrotTrouble(false, 6));
+	}
+
+	@Test
+	public void testMakes10() {
+		assertTrue(obj.makes10(9, 10));
+		assertFalse(obj.makes10(9, 9));
+		assertTrue(obj.makes10(1, 9));
+	}
+
+	@Test
+	public void testNearHundred() {
+		assertTrue(obj.nearHundred(93));
+		assertTrue(obj.nearHundred(90));
+		assertFalse(obj.nearHundred(89));
+	}
+
+	@Test
+	public void testPosNeg() {
+		assertTrue(obj.posNeg(1, -1, false));
+		assertTrue(obj.posNeg(-1, 1, false));
+		assertTrue(obj.posNeg(-4, -5, true));
+	}
+	
+	@Test
+	public void testNotString() {
+		assertEquals("not candy", obj.notString("candy"));
+		assertEquals("not x", obj.notString("x"));
+		assertEquals("not bad", obj.notString("not bad"));
+	}
+	
+	@Test
+	public void testMissingChar() {
+		assertEquals("ktten", obj.missingChar("kitten", 1));
+		assertEquals("itten", obj.missingChar("kitten", 0));
+		assertEquals("kittn", obj.missingChar("kitten", 4));
+	}
+	
+	@Test
+	public void testFrontBack() {
+		assertEquals("eodc", obj.frontBack("code"));
+		assertEquals("a", obj.frontBack("a"));
+		assertEquals("ab", obj.frontBack("ba"));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
