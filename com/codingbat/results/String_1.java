c35a33ed259678a9a44a94f5ccb21dfbc444ba14
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-09-22 22:31:34 -0400

    added more exercises

--- com/codingbat/results/String_1.java
+++ com/codingbat/results/String_1.java
@@ -4,5 +4,13 @@ public class String_1 {
 	
 	public String helloName(String name) {
 		return "Hello " + name + "!";
-	}	
+	}
+	
+	public String makeAbba(String a, String b) {
+		return a + b + b + a;
+	}
+	
+	public String makeTags(String tag, String word) {
+		return "<" + tag + ">" + word + "</" + tag + ">";
+	}
 }
--- com/codingbat/results/String_1Test.java
+++ com/codingbat/results/String_1Test.java
@@ -14,5 +14,18 @@ public class String_1Test {
 		assertEquals("Hello Alice!", obj.helloName("Alice"));
 		assertEquals("Hello X!", obj.helloName("X"));
 	}
-
+	
+	@Test
+	public void testMakeAbba() {
+		assertEquals("HiByeByeHi", obj.makeAbba("Hi", "Bye"));
+		assertEquals("YoAliceAliceYo", obj.makeAbba("Yo", "Alice"));
+		assertEquals("WhatUpUpWhat", obj.makeAbba("What", "Up"));
+	}
+	
+	@Test
+	public void testMakeTags() {
+		assertEquals("<i>Yay</i>", obj.makeTags("i", "Yay"));
+		assertEquals("<i>Hello</i>", obj.makeTags("i", "Hello"));
+		assertEquals("<cite>Yay</cite>", obj.makeTags("cite", "Yay"));
+	}
 }
